75dc55a39d2e8748cb0fb5a5e0815764f762f423
shockingsrose <847073376@qq.com>
2018-04-18 18:39:43 +0800

    123

--- routes/detail.js
+++ routes/detail.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-var list = [1, 2, 3];
+var list = [1, 2, 3, 4];
 router.get('/', (req, res, next) => {
-  res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' });
+  res.writeHead(200, {
+    'Content-Type': 'application/json;charset=utf-8'
+  });
   let data = JSON.stringify(list);
   var callback = 'callbackFunction' + '(' + data + ');';
   res.end(callback);
